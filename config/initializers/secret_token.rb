d2d95471fb1f19e3461fc5449d5a1ab7ee28792c
Andrey Dmitrichev <dmitrichev@gmail.com>
2012-10-24 23:57:21 +0400

    first cool commit - for best tranings app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hundredsteps::Application.config.secret_token = 'e4ef0bc918dc08fe487495bf708ec78e5729ec8663dc91309f590c44aedeef945eeae6be78daf224e0dafdc82f91ad13e8a4ae0fdad6671da9bb98c0ff24cb74'
